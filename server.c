1c77c589fce53ef9dcd155fdde8d6e471257a7f9
Adam Cabe <guitarnut27@gmail.com>
2020-02-18 13:45:57 -0500

    first commit

---
+++ server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+
+int main() 
+{
+    printf("This is going to be the server\n");
+
+    return 0;
+}
